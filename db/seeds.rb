f1c038052f68108643921676bb429697c371c6b8
Ben Pham <benjamin.pham@gmail.com>
2019-02-21 12:31:38 +0000

    Created all models, routes and controllers + basic views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+rhum = Ingredient.create(name: "rhum")
+perrier = Ingredient.create(name: "perrier")
+mint = Ingredient.create(name: "mint leaves")
+
+mojito = Cocktail.create(name: "mojito")
+
+Dose.create(description: "4cl", ingredient: rhum, cocktail: mojito)
+Dose.create(description: "8cl", ingredient: perrier, cocktail: mojito)
+Dose.create(description: "4 leaves", ingredient: mint, cocktail: mojito)
