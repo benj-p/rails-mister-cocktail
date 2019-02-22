d65f92974c6f41f0f6cf9144e307d26dad5d3349
Ben Pham <benjamin.pham@gmail.com>
2019-02-22 11:16:24 +0000

    removed boostrap import from js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
-
-import { initSelect2, select2Instance } from '../components/form';
+import { initSelect2 } from '../components/form';
 
 initSelect2();
